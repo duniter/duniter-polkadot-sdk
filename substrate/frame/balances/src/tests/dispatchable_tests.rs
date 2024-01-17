2f99cb8af4b4ca6c839fe125d75fdd90040631ca
bgallois <benjamin@gallois.cc>
2024-01-17 16:20:26 +0100

    remove pallet-balances upgrade_account extrinsic
    Remove the upgrade_account extrinsic that cannot be integrated with the
    duniter_account AccountData.

--- substrate/frame/balances/src/tests/dispatchable_tests.rs
+++ substrate/frame/balances/src/tests/dispatchable_tests.rs
@@ -195,7 +195,7 @@ fn set_balance_handles_total_issuance() {
 	});
 }
 
-#[test]
+/*#[test]
 fn upgrade_accounts_should_work() {
 	ExtBuilder::default()
 		.existential_deposit(1)
@@ -228,7 +228,7 @@ fn upgrade_accounts_should_work() {
 			assert_eq!(System::providers(&7), 0);
 			assert_eq!(System::consumers(&7), 0);
 		});
-}
+}*/
 
 #[test]
 #[docify::export]
